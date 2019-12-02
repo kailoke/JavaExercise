e24129b747d443992a0a475395dd242a17802730
ShavenF <f476947408@163.com>
2019-12-02 11:30:32 +0800

    modify

--- Socket/src/a4_URL/URLTest.java
+++ Socket/src/a4_URL/URLTest.java
@@ -7,6 +7,8 @@ import java.net.URL;
  * 一、格式
  *  > 通信协议 :// 主机名(域名) : 端口号 / 资源地址 # 片段名 ? 参数列表
  *
+ * URL下载流程
+ * 1.实例化URL对象   2.获取URL连接协议对象 3.连接对象建立连接  4.获取连接对象的输入流
  */
 public class URLTest {
     public static void main(String[] args) throws MalformedURLException {
