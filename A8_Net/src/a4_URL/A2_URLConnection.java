62058b5343b1eb70aab69e63a7a1218958adbf09
LenovoX260 <f476947408@163.com>
2020-01-09 15:58:13 +0800

    test download webPage's content

--- A8_Net/src/a4_URL/A2_URLConnection.java
+++ A8_Net/src/a4_URL/A2_URLConnection.java
@@ -2,7 +2,9 @@ package a4_URL;
 
 import org.junit.Test;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URL;
 import java.net.URLConnection;
 
@@ -21,14 +23,30 @@ import java.net.URLConnection;
  * public OutputStream getOutputStream( )throws IOException
  *
  * 三、URL下载流程
- *  1.实例化URL对象  2.获取URL连接协议对象   3.连接对象建立连接  4.获取连接对象的输入流
+ *  1.实例化URL对象  2.获取URL连接协议对象(连接对象建立连接)  3.获取连接对象的输入流
  */
 
 
 public class A2_URLConnection {
     @Test
     public void urlConnection() throws IOException {
-        URL url = new URL("http://www.atguigu.com/index.shtml");
+        URL url = new URL("https://www.sina.com.cn/");
         URLConnection conn = url.openConnection( );
+
+        long date = conn.getDate();
+        System.out.println("conn.getDate(): " + date);
+
+        Object content = conn.getContent();
+        System.out.println(content);
+
+        // 获取网页内容
+        InputStream inputStream = conn.getInputStream();
+        byte[] buff = new byte[1024];
+        ByteArrayOutputStream bais = new ByteArrayOutputStream();
+        int len;
+        while ((len = inputStream.read(buff)) != -1) {
+            bais.write(buff,0,len);
+        }
+        System.out.println(bais.toString());
     }
 }
